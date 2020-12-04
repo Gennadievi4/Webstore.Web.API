4b03b020476f2b05d601355317e39c9776b248e2
Геннадий Тертычный <gonzyck@gmail.com>
2020-12-04 16:09:31 +0400

    Добавил контроллер сотрудника

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Models;
+using WebStore.Services;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+
+        private readonly List<Employee> _Db;
+        public EmployeesController(DbInMemory db) => _Db = db._Employees;
+        public IActionResult Index() => View(_Db);
+        public IActionResult Details(int id)
+        {
+            var employee = _Db.FirstOrDefault(x => x.Id == id);
+            if (employee is not null)
+                return View(employee);
+            return NotFound();
+        }
+    }
+}
