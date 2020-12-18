b523b75e4ee624514057ff1a61f07ef018e1044a
Геннадий Тертычный <gonzyck@gmail.com>
2020-12-18 16:49:14 +0400

    подготовил Вью входа в магазин, сделал контроллер для Вью входа в магазин

--- WebStore/Controllers/LoginController.cs
+++ WebStore/Controllers/LoginController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebStore.Domain.Identity;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
-    public class LoginController : Controller
+    public class Login : Controller
     {
-        public IActionResult LoginIndex()
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+        public Login(UserManager<User> UserManager, SignInManager<User> SignInManager)
         {
-            return View();
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
+        }
+
+        public IActionResult Register() => View(new RegisterViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel Model)
+        {
+            if (!ModelState.IsValid)
+                return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName,
+            };
+
+            var registration_result = await _UserManager.CreateAsync(user, Model.Password);
+            if (registration_result.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registration_result.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(Model);
+
         }
     }
 }
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using WebStore.DAL.Context;
 using WebStore.Data;
+using WebStore.Domain.Identity;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Infrastructure.Middleware;
 using WebStore.Infrastructure.Services.InMemory;
@@ -25,6 +28,41 @@ namespace WebStore
         {
             services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(_Configuration.GetConnectionString("Default")));
             services.AddTransient<WebStoreDbInitializer>();
+
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<WebStoreDB>()
+                .AddDefaultTokenProviders();
+
+#if DEBUG
+            services.Configure<IdentityOptions>(opt =>
+            {
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredUniqueChars = 3;
+#endif
+                opt.User.RequireUniqueEmail = false;
+                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+                opt.Lockout.AllowedForNewUsers = false;
+                opt.Lockout.MaxFailedAccessAttempts = 10;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+            });
+
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.Cookie.Name = "WebStore";
+                opt.Cookie.HttpOnly = true;
+                opt.ExpireTimeSpan = TimeSpan.FromDays(10);
+
+                opt.LoginPath = "/Account/Login";
+                opt.LogoutPath = "Account/Logout";
+                opt.AccessDeniedPath = "/Account/AccessDenied";
+
+                opt.SlidingExpiration = true;
+            });
 
             services.AddMvc(opt =>
                 {
@@ -48,6 +86,9 @@ namespace WebStore
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseWelcomePage("/Welcom");
 
