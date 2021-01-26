44fee922a93e3e2719464222caa6eefb67363439
Геннадий Тертычный <gonzyck@gmail.com>
2021-01-26 15:20:37 +0300

    Добавил проект Web.Api. Начало формирования web-служб.

---
+++ Services/WebStore.ServiceHosting/Controller/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ServiceHosting.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly List<string> __Values = Enumerable
+            .Range(1, 10)
+            .Select(i => $"Value {i:00}")
+            .ToList();
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return __Values;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
