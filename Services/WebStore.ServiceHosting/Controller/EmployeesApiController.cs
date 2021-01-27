1cab07a1123d9a2f4971b85f8b3f92bffc605d8c
Геннадий Тертычный <gonzyck@gmail.com>
2021-01-27 14:48:57 +0300

    Перенёс сервисы в startup WebHosting'a, создал первый WebApiController

---
+++ Services/WebStore.ServiceHosting/Controller/EmployeesApiController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Domain.Models;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controller
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesApiController : ControllerBase, IEmployeesData
+    {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesApiController(IEmployeesData EmployeesData)
+        {
+            _EmployeesData = EmployeesData;
+        }
+
+        [HttpPost]
+        public int Add(Employee emp)
+        {
+            return _EmployeesData.Add(emp);
+        }
+
+        [HttpDelete("{id}")]
+        public bool Delete(int id)
+        {
+            return _EmployeesData.Delete(id);
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _EmployeesData.Get();
+        }
+
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return _EmployeesData.Get(id);
+        }
+
+        [HttpPut]
+        public void Update(Employee emp)
+        {
+            _EmployeesData.Update(emp);
+        }
+    }
+}
--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -1,22 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 using WebStore.Clients.Values;
-using WebStore.DAL.Context;
-using WebStore.Domain.Identity;
 using WebStore.Infrastructure.Middleware;
-using WebStore.Interfaces.Services;
 using WebStore.Interfaces.TestApi;
 using WebStore.Services.Data;
-using WebStore.Services.Products.InCookies;
-using WebStore.Services.Products.InMemory;
-using WebStore.Services.Products.InSQL;
 
 namespace WebStore
 {
@@ -29,54 +20,12 @@ namespace WebStore
         }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(_Configuration.GetConnectionString("Default")));
-            services.AddTransient<WebStoreDbInitializer>();
-
-            services.AddIdentity<User, Role>()
-                .AddEntityFrameworkStores<WebStoreDB>()
-                .AddDefaultTokenProviders();
-
-#if DEBUG
-            services.Configure<IdentityOptions>(opt =>
-            {
-                opt.Password.RequiredLength = 3;
-                opt.Password.RequireDigit = false;
-                opt.Password.RequireLowercase = false;
-                opt.Password.RequireUppercase = false;
-                opt.Password.RequireNonAlphanumeric = false;
-                opt.Password.RequiredUniqueChars = 3;
-#endif
-                opt.User.RequireUniqueEmail = false;
-                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-
-                opt.Lockout.AllowedForNewUsers = false;
-                opt.Lockout.MaxFailedAccessAttempts = 10;
-                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
-            });
-
-            services.ConfigureApplicationCookie(opt =>
-            {
-                opt.Cookie.Name = "WebStore";
-                opt.Cookie.HttpOnly = true;
-                opt.ExpireTimeSpan = TimeSpan.FromDays(10);
-
-                opt.LoginPath = "/Login/SignInUser";
-                opt.LogoutPath = "/Login/SignOutUser";
-                opt.AccessDeniedPath = "/Login/AccessDenied";
-
-                opt.SlidingExpiration = true;
-            });
-
             services.AddMvc(opt =>
                 {
                     opt.Conventions.Add(new WebStoreControllerConvention());
                 })
                 .AddRazorRuntimeCompilation();
 
-            services.AddSingleton<IEmployeesData, DbInMemory>();
-            services.AddTransient<IProductData, SqlProductData>();
-            services.AddScoped<ICartServices, InCookiesCartService>();
-            services.AddScoped<IOrderService, SqlOrderService>();
             services.AddScoped<IValuesService, ValuesClient>();
 
             services.AddControllersWithViews()
