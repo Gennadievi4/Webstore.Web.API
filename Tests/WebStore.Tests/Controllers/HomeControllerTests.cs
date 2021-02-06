61d1fef42e17c4e62b3fdd104b5db42cf0d916ab
Геннадий Тертычный <gonzyck@gmail.com>
2021-02-06 15:51:49 +0300

    Начало разбора модульного тестирования

---
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStore.Controllers;
+
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index_Returns_View()
+        {
+            //A-A-A = Arrange - Act - Assert = Подготовка данных - выполнение действия - проверка результатов
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            //Assert.IsInstanceOfType(result, typeof(ViewResult)); - MSTest
+        }
+    }
+}
